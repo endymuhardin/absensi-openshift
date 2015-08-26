1574e1ad434c5cddcb4521cc14b27ec093004477
Endy Muhardin <endy.muhardin@gmail.com>
2015-08-26 15:19:36 +0700

    controller untuk save karyawan

--- src/main/java/com/artivisi/absensi/controller/KaryawanController.java
+++ src/main/java/com/artivisi/absensi/controller/KaryawanController.java
@@ -5,7 +5,11 @@ import com.artivisi.absensi.entity.Karyawan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,5 +20,11 @@ public class KaryawanController {
     @RequestMapping("/karyawan/list")
     public Page<Karyawan> semua(Pageable page){
         return karyawanDao.findAll(page);
+    }
+    
+    @RequestMapping(value = "/karyawan/", method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.CREATED)
+    public void create(@RequestBody Karyawan k){
+        karyawanDao.save(k);
     }
 }
