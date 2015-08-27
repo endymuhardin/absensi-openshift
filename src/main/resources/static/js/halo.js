22284bc96b9c29ce3a994dcf697f5f28f55994f1
Endy Muhardin <endy.muhardin@gmail.com>
2015-08-27 14:39:16 +0700

    implement angular HTTP Get

--- src/main/resources/static/js/halo.js
+++ src/main/resources/static/js/halo.js
@@ -16,3 +16,17 @@ myApp.controller('EmailController', ['$scope', function($scope) {
       $scope.daftarEmail.splice(indexOfX, 1);
   };
 }]);
+
+myApp.controller('KaryawanController', ['$scope', '$http', function($scope, $http){
+    $scope.updateTabelKaryawan = function(){
+        var promise = $http.get('/karyawan/list');
+        promise.then(
+            function(responSukses){
+                $scope.daftarKaryawan = responSukses.data;
+            }, 
+            function(responGagal){
+                alert("Error Status : "+responGagal.status);
+            }
+        );
+    };
+}]);
